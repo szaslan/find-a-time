6db2379336c8fd9008a9fa5ea24236388142b100
Zack <szaslan@gmail.com>
2020-01-11 20:07:17 -0600

    moved constants to separate file

--- src/components/SchedulerEntry.js
+++ src/components/SchedulerEntry.js
@@ -1,38 +1,32 @@
 import React, { useEffect, useState } from "react";
 // need to import database as db
-
-const hours = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
-const minutes = ['00', '30']
-const dates = ['M', 'Tu', 'W', 'Th', 'F']; // replace this with the dates, try to get from gcal
-
+import { hours, minutes, dates } from "../constants";
 //1. create an empty schedule entry for each data/time option
 //2. find the schedule based off id from database, compare times
 
-//for each 
+//for each
 const ScheduleEntry = () => {
-    const times = createTimes();
-    const events = dates.map(function(item){
-        return {item, times};})
-        //return createDateEntry(item, times);})
-    return events;
-    //const schedule = `"id": ${id}, "events": ${events} `
-    //saveToFirebase(schedule);
-
-}
-
-
+  const times = createTimes();
+  const events = dates.map(function(item) {
+    return { item, times };
+  });
+  //return createDateEntry(item, times);})
+  return events;
+  //const schedule = `"id": ${id}, "events": ${events} `
+  //saveToFirebase(schedule);
+};
 
 //this creates every possible hour/minute combindation
 const createTimes = () => {
-    return hours.map(function (item) {
-        return minutes.map(function (item2) {
-            return `${item}:${item2}:00`;
-        })
-    }).flat()
-}
+  return hours
+    .map(function(item) {
+      return minutes.map(function(item2) {
+        return `${item}:${item2}:00`;
+      });
+    })
+    .flat();
+};
 
-const saveToFirebase = ({ schedule }) => {
-
-}
+const saveToFirebase = ({ schedule }) => {};
 
 export default ScheduleEntry;
---
+++ src/constants.js
@@ -0,0 +1,27 @@
+export const hours = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23"
+];
+export const minutes = ["00", "30"];
+export const dates = ["M", "Tu", "W", "Th", "F"]; // replace this with the dates, try to get from gcal
