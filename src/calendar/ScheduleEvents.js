2b58fdfa715d7ffca6dee1c12158695607927ecc
jbrain98 <33076782+jbrain98@users.noreply.github.com>
2020-01-18 16:48:24 -0600

    Update ScheduleEvents.js

--- src/calendar/ScheduleEvents.js
+++ src/calendar/ScheduleEvents.js
@@ -1,76 +1,84 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import UpdateDb from "../Db/UpdateDb";
-var moment = require('moment');
+import moment from 'moment'
 
 
+const minutes = ['00', '30'];
 
-const hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
-const minutes = ['00', '30']
-const dates = ['M', 'Tu', 'W', 'Th', 'F']; // replace this with the dates, try to get from gcal
+//roomId, date, interval, userName, isBusy
+const AddEvents = (roomId, userName, events) => {
+    events.map(event => findIntervals(roomId, userName, event));
+};
 
-
-//eventId, date, interval, userName, isBusy
-const AddEvents = (eventId, userName, events) => {
-    events.map(event =>
-        findIntervals(eventId, userName, event))
-}
-
-const findIntervals = (eventId, userName, event) => {
-    const start = getStartTime(event);
-    const end = getEndTime(event);
-    findBuckets(eventId, userName, start, end);
-}
 
 const getStartTime = (event) => {
-    var start = event.start.dateTime;
-    if (!start){
+    let start = event.start.dateTime;
+    if (!start) {
         start = event.start.date;
     }
     return moment(start);
-}
+};
 
 const getEndTime = (event) => {
-    var end = event.end.dateTime;
-    if (!end){
+    let end = event.end.dateTime;
+    if (!end) {
         end = event.end.date;
-    }    
+    }
     return moment(end);
-}
+};
 
 const getDay = (time) => {
-    time = time.toISOString().split('T', 1);
-    return time
-}
+    return time.format('YYYY-MM-DD')
+};
 
-const findBuckets = (eventId, userName, curr, end) => {
-    const endMin = end.get('minute');
-    const endHour = end.get('hour');
-    var i = 0;
-    while (i < minutes.length){
-        if (minutes[i] <= curr.get('minute') < minutes[i + 1]){
-            break
+const findIntervals = (roomId, userName, event) => {
+    const start = getStartTime(event);
+    const end = getEndTime(event);
+    findBuckets(roomId, userName, start, end);
+};
+
+const findBuckets = (roomId, userName, currTime, endTime) => {
+
+    console.log('start time');
+    console.log(currTime);
+    console.log('endtime');
+    console.log(endTime);
+    // Finding the first time interval -- figuring out what minute to start at
+    // if min is in [0,30) then set i=0, otherwise set i=1.
+    var i = 0
+
+    while (i < minutes.length - 1){
+        if (Number(minutes[i]) <= currTime.minute() && currTime.minute() < Number(minutes[i + 1])){
+            currTime.minutes(Number(minutes[i]));
+            break;
         }
         i += 1;
     }
 
-    //console.log("Finding intervals");
-    let returnList = [];
-    let payload;
-    while (!(curr.get('day') == end.get('day') && curr.get('hour') == endHour && minutes[i] > endMin)){
-       payload = {"eventId":eventId,"date":getDay(curr), "userName":userName, "interval":`${curr.get('hour')}:${minutes[i]}`, "isBusy": true };
-        returnList.push(payload);
-        console.log("hey");
-       // UpdateDb(payload);
-        i += 1;
-        if (i == minutes.length){
-            curr.add(1, 'h');
-            i = 0;
-        }        
-    }
-    console.log("length of list: "+ returnList.length);
-    returnList.forEach((item)=>{ UpdateDb(item)});
+    // keep incrementing startTime by 30min until startTime>= endTime then stop,
+    // for each day record the 30min intervals seen in `dayPayload`,
+    // once the day changes add `dayPayload` to `payload`
+    let currentDay=  getDay(currTime);
+    let dayPayload = {};
+    let payload = {};
 
+    while (currTime.isBefore(endTime)) {
+        dayPayload[`${currTime.hour()}:${minutes[i]}`] = 1;
+        i = (i+1)%(minutes.length);
+        currTime.add(Number(minutes[1]),'minutes');
 
-}
+        if (getDay(currTime) !== currentDay){
+            payload[currentDay] = dayPayload;
+            currentDay = getDay(currTime);
+            dayPayload = {};
+        }
+    }    
+
+    payload[currentDay]=dayPayload;
+
+    // the actual payload w/ identifying information
+    const actualPayload = {userName:userName, roomId:roomId, data: payload};
+    console.log(actualPayload);
+};
 
 export default AddEvents;
